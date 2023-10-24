2e13f5d6318d3b218bc056f1700fe7691b5690d4
notAvailable73 <mainulhasan@iut-dhaka.edu>
2023-10-25 05:18:33 +0600

    fixed some error

--- SPL_PROJECT/SPL_PROJECT/Program.cs
+++ SPL_PROJECT/SPL_PROJECT/Program.cs
@@ -13,8 +13,11 @@ namespace SPL_PROJECT
         static void Main(string[] args)
         {
             Starter starter = new Starter();
-            utility.mainMenu();
-
+            //utility.mainMenu();
+            user ask = new user("ria", "ra", "123", "asd", DateTime.Today);
+            ElectronicProducts ep = new ElectronicProducts(1001, "a", 1234, "a");
+            ask.addToCart(ep);
+            ask.cart.showCart();
             Console.ReadLine();
         }
     }
