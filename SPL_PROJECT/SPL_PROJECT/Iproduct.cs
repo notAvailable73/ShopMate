4f8babc28aed1ffbe9010a6426bdb52f02dbd5d7
Kashshaf-Labib <labibkashshaf@gmail.com>
2023-12-29 19:00:31 +0600

    Refactored Browse Products

--- SPL_PROJECT/SPL_PROJECT/Iproduct.cs
+++ SPL_PROJECT/SPL_PROJECT/Iproduct.cs
@@ -1,35 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SPL_PROJECT
 {
     public interface IProduct
     {
-         int id { get; set; }
-         string name { get; set; }
-         double price { get; set; }
-         string description { get; set; }
+        int id { get; set; }
+        string name { get; set; }
+        double price { get; set; }
+        string description { get; set; }
+        void DisplayDetails();
     }
 
-    public class ElectronicProducts:IProduct
+    public class ElectronicProducts : IProduct
     {
         public int id { get; set; }
         public string name { get; set; }
         public double price { get; set; }
         public string description { get; set; }
-        public ElectronicProducts(int id, string name, double price, string description) 
+        public ElectronicProducts(int id, string name, double price, string description)
         {
             this.id = id;
             this.name = name;
             this.price = price;
             this.description = description;
         }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Price: {price}");
+            Console.WriteLine($"Description: {description}");
+            Console.WriteLine();
+        }
     }
 
-    public class Cloth: IProduct
+    public class Cloth : IProduct
     {
         public int id { get; set; }
         public string name { get; set; }
@@ -42,9 +53,16 @@ namespace SPL_PROJECT
             this.price = price;
             this.description = description;
         }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Price: {price}");
+            Console.WriteLine($"Description: {description}");
+            Console.WriteLine();
+        }
     }
 
-    public class HomeAppliences: IProduct
+    public class HomeAppliences : IProduct
     {
         public int id { get; set; }
         public string name { get; set; }
@@ -56,6 +74,13 @@ namespace SPL_PROJECT
             this.name = name;
             this.price = price;
             this.description = description;
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Price: {price}");
+            Console.WriteLine($"Description: {description}");
+            Console.WriteLine();
         }
     }
 }
